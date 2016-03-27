438acfd1c692ccf5310a8d63f9c18bdbfd5ce45c
bfchengnuo <bfchengnuo@gmail.com>
2016-03-27 21:49:59 +0800

    listviewtext补充

--- code/listview列表待解决--已解决/ListviewText/app/src/main/java/listviewtext/listviewtext/MainActivity.java
+++ code/listview列表待解决--已解决/ListviewText/app/src/main/java/listviewtext/listviewtext/MainActivity.java
@@ -2,17 +2,22 @@ package listviewtext.listviewtext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-
-public class MainActivity extends AppCompatActivity {
+//实现监听手指滑动的接口
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener,AbsListView.OnScrollListener {
 
     private ListView listView;
 //    定义数组适配器
@@ -52,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
 //        form中的键名和to中的ID一定要对应起来！！！就在这里被坑了  最后两个
         simp_ter = new SimpleAdapter(this,getDateList(),R.layout.datelayout,new String[] {"src","text"},new int[] {R.id.src,R.id.text});
         listView.setAdapter(simp_ter);
+        // 设置ListView的元素被选中时的事件处理监听器
+        listView.setOnItemClickListener(this);
+        listView.setOnScrollListener(this);
     }
 
     private List<Map<String,Object>> getDateList(){
@@ -65,5 +73,72 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return dateList;
+    }
+
+    @Override
+    //列表内容被点击后事件
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        获取列表控件的信息，位置和内容
+        String text = listView.getItemAtPosition(position)+"";
+//        将获取的内容显示在屏幕,,上下文对象，文本内容，显示时间
+        Toast.makeText(MainActivity.this,"位置："+position+"内容："+text,Toast.LENGTH_LONG).show();
+    }
+
+//    监听手势滑动
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+        switch (scrollState){
+            case SCROLL_STATE_FLING :
+//                手指离开屏幕之前，由于惯性继续滑动
+                Toast.makeText(MainActivity.this,"用力划了一下",Toast.LENGTH_LONG).show();
+                Map<String,Object> map = new HashMap<String, Object>();
+                map.put("text","滚动添加的数据");
+                map.put("src",R.drawable.demo);
+                dateList.add(map); //将数据添加到数据源
+//            将数据添加到简单适配器
+//                不需要添加  否则会重置列表到顶部
+               // listView.setAdapter(simp_ter);
+
+//            更新UI
+                simp_ter.notifyDataSetChanged();
+                break;
+            case SCROLL_STATE_IDLE :
+//                已经停止滑动
+                break;
+            case SCROLL_STATE_TOUCH_SCROLL :
+//                手指没有离开屏幕，视图正在滑动
+                break;
+        }
+
+
+
+        /*  另一种方式
+
+//        手指离开屏幕前，用力划了一下
+        if(scrollState == SCROLL_STATE_FLING){
+            Toast.makeText(MainActivity.this,"用力划了一下",Toast.LENGTH_LONG).show();
+
+            Map<String,Object> map = new HashMap<String, Object>();
+            map.put("text","滚动添加的数据");
+            map.put("src",R.drawable.demo);
+            dateList.add(map); //将数据添加到数据源
+//            将数据添加到简单适配器
+            listView.setAdapter(simp_ter);
+//            更新UI
+            simp_ter.notifyDataSetChanged();
+        }else if(scrollState == SCROLL_STATE_IDLE){
+            //停止滚动时
+
+        }else if(scrollState == SCROLL_STATE_TOUCH_SCROLL){
+            //正在滚动的时候
+        }
+    */
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
     }
 }
